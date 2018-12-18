a5d3df3abce4ce0b11998a3c6a723496aef7001e
hannah <36429830+hanpham32@users.noreply.github.com>
2018-12-18 09:32:42 -0800

    Exercise 1+2 Done

--- Unit4/src/Main.java
+++ Unit4/src/Main.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		Point point1 = new Point();
-		Point point2 = new Point();
-		point1.setPoint(2, 2);
-		point2.setPoint(4, 5);
+		Scanner in = new Scanner(System.in);
+		Point point1 = new Point(in.nextInt(),in.nextInt());
+		Point point2 = new Point(in.nextInt(),in.nextInt());
 
-		System.out.println(point1.findDistance(point1, point2));
+		System.out.println(point1.findPolarCoordinates(point1, point2));
+		System.out.println("Point 1: "+ point1.getPoint());
+		System.out.println("Point 2: "+ point2.getPoint());
 	}
 }
--- Unit4/src/Point.java
+++ Unit4/src/Point.java
@@ -4,6 +4,10 @@ public class Point {
 	private int x = 0;
 	private int y = 0;
 	
+	public Point(int x, int y) {
+		setPoint(x,y);
+	}
+	
 	public void setPoint(int x, int y) {
 		this.x = x;
 		this.y = y;
@@ -15,14 +19,18 @@ public class Point {
 	public int getY() {
 		return y;
 	}
+	public String getPoint() {
+		return "("+x+","+y+")";
+	}
 	
-	public String findDistance(Point one, Point two) {
+	public String findPolarCoordinates(Point one, Point two) {
+		DecimalFormat df = new DecimalFormat("#.##");
 		int x1 = one.getX();
 		int x2 = two.getX();
 		int y1 = one.getY();
 		int y2 = two.getY();
-		double distance = Math.sqrt(Math.pow(2,x2-x1) + Math.pow(2, y2-y1));
-		DecimalFormat df = new DecimalFormat("#.##");
-		return df.format(distance);
+		double distance = Math.sqrt(Math.pow(2,x2-x1) + Math.pow(2, y2-y1));	
+		double angle = Math.toDegrees(Math.atan((y2-y1)/(x2-x1)));
+		return "("+df.format(distance)+","+angle+"°)";
 	}
 }
