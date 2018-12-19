c0aa6d6ddb7fb9f85f4e4e28ea33aeeff40dc093
hannah <36429830+hanpham32@users.noreply.github.com>
2018-12-19 08:47:18 -0800

    Exercise 3

--- Unit4/src/Point.java
+++ Unit4/src/Point.java
@@ -34,4 +34,23 @@ public class Point {
 		double angle = Math.toDegrees(Math.atan((y2-y1)/(x2-x1)));
 		return "("+df.format(distance)+","+angle+"°)";
 	}
+	
+	public double distanceTo(Point one, Point two) {
+		int x1 = one.getX();
+		int x2 = two.getX();
+		int y1 = one.getY();
+		int y2 = two.getY();
+		return Math.sqrt(Math.pow(2,x2-x1) + Math.pow(2, y2-y1));
+	}
+	
+	public String slopeTo(Point one, Point two) {
+		int x1 = one.getX();
+		int x2 = two.getX();
+		int y1 = one.getY();
+		int y2 = two.getY();
+		int changeInY = y2 - y1;
+		int changeInX = x2 - x1;
+		
+		return changeInY+"/"+changeInX;
+	}
 }
