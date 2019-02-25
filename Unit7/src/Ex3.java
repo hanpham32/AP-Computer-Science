d2e8ec38c6c7a8070546abfda117669351b91499
Hannah Pham <36429830+hanpham32@users.noreply.github.com>
2019-02-25 09:30:09 -0800

    ex3

--- Unit7/src/Ex3.java
+++ Unit7/src/Ex3.java
@@ -1,4 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Ex3 {
-
+	public static void main(String[] args) throws FileNotFoundException {
+		File f = new File("C:\\Users\\s-phamh\\Documents\\GitHub\\AP_Comp_Sci_Unit_Exercises\\Unit7\\src\\numbers.txt");
+		Scanner scan = new Scanner(f);
+		
+	}
 }
