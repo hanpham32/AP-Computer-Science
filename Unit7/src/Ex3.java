244ec7985d852ef32514ffc786c2af5f4965e80d
Hannah Pham <36429830+hanpham32@users.noreply.github.com>
2019-02-25 09:46:57 -0800

    Ex3

--- Unit7/src/Ex3.java
+++ Unit7/src/Ex3.java
@@ -1,11 +1,34 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ex3 {
 	public static void main(String[] args) throws FileNotFoundException {
 		File f = new File("C:\\Users\\s-phamh\\Documents\\GitHub\\AP_Comp_Sci_Unit_Exercises\\Unit7\\src\\numbers.txt");
 		Scanner scan = new Scanner(f);
+		ArrayList<Integer> list = new ArrayList();
+		while (scan.hasNextLine()) {
+			list.add(Integer.parseInt(scan.nextLine()));
+		}
+		int smallest_index = 0;
+		int j = 1;
+		for (int i=0; i<list.size(); i++) {
+			if (list.get(smallest_index) > list.get(j)) {
+				swap(list, smallest_index,j);
+			} else {
+				j++;
+			}
+		}
 		
+		for (int x : list) {
+			System.out.print(x + " ");
+		}
+	}
+	
+	public static void swap(ArrayList<Integer> l, int smallest, int i) {
+		int temp = smallest;
+		l.set(smallest, i);
+		l.set(i, temp);
 	}
 }
