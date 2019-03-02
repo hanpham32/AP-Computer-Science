858220ca1765494bb0aeff6ed90866241c35d1c5
Han <36429830+hanpham32@users.noreply.github.com>
2019-03-01 22:29:05 -0800

    Finished Ex3

--- Unit7/src/Ex3.java
+++ Unit7/src/Ex3.java
@@ -5,38 +5,47 @@ import java.util.Scanner;
 
 public class Ex3 {
 	public static void main(String[] args) throws FileNotFoundException {
-		File f = new File("C:\\Users\\s-phamh\\Documents\\GitHub\\AP_Comp_Sci_Unit_Exercises\\Unit7\\src\\names.txt");
+		File f = new File("C:\\Users\\yunir\\eclipse-workspace\\AP_Comp_Sci_Unit_Exercises\\Unit7\\src\\names.txt");
 		Scanner scan = new Scanner(f);
 		ArrayList<String> list = new ArrayList();
 		while (scan.hasNextLine()) {
 			list.add(scan.nextLine());
 		}
-		
 		for (String x : list) {
-			System.out.println(x);
+			System.out.print(x + " ");
+		} System.out.println();
+		Scanner in = new Scanner(System.in);
+		String user = in.nextLine();
+		if (user.equals("Ascending")) {
+			Ascending(list);
+		} else if (user.equals("Descending")) {
+			Descending(list);
 		}
-		
+	}
+	
+	public static void Descending(ArrayList<String> list) {
 		for (int i=1; i<list.size(); i++) {
 			for (int j=i; j>0 && list.get(j).compareTo(list.get(j-1)) >= 0; j--) {
 				String temp = list.get(j);
 				list.set(j, list.get(j-1));
 				list.set(j-1, temp);
 			}
-			for (String x : list) {
-				System.out.print(x+" ");
-			} System.out.println();
 		}
-		
-//		for (int i=1; i<list.size();i++) {
-//			for (int j=i; j>0 && list.get(j).compareTo(list.get(j-1)) <= 0; j--) {
-//				String temp = list.get(j);
-//				list.set(j, list.get(j-1));
-//				list.set(j-1, temp);
-//			}
-//		}
-//		
-//		for (String x : list) {
-//			System.out.print(x+" ");
-//		}		
+		for (String x : list) {
+			System.out.print(x+" ");
+		} System.out.println();
+	}
+	
+	public static void Ascending(ArrayList<String> list) {
+		for (int i=1; i<list.size();i++) {
+			for (int j=i; j>0 && list.get(j).compareTo(list.get(j-1)) <= 0; j--) {
+				String temp = list.get(j);
+				list.set(j, list.get(j-1));
+				list.set(j-1, temp);
+			}
+		}
+		for (String x : list) {
+			System.out.print(x+" ");
+		} System.out.println();
 	}
 }
