c36af70c48b105b78449c38990dad0bded359e85
Hannah Pham <36429830+hanpham32@users.noreply.github.com>
2019-03-04 09:39:49 -0800

    Finished ex3 unit 7

--- Unit7/src/Ex3.java
+++ Unit7/src/Ex3.java
@@ -5,39 +5,26 @@ import java.util.Scanner;
 
 public class Ex3 {
 	public static void main(String[] args) throws FileNotFoundException {
+		File f = new File("C:\\Users\\s-phamh\\Documents\\GitHub\\AP_Comp_Sci_Unit_Exercises\\Unit7\\src\\names.txt");
 		Scanner scan = new Scanner(f);
 		ArrayList<String> list = new ArrayList();
 		while (scan.hasNextLine()) {
 			list.add(scan.nextLine());
 		}
-<<<<<<< HEAD
-		
-		Scanner user = new Scanner(System.in);
-		if (user.nextLine().equals("Accending")) {
-			Accending(list);
-		} else if (user.nextLine().equals("Decending")) {
-			Decending(list);
-		}
-		
-=======
->>>>>>> f6abc7ae51ed6e2944d1b890da60f4fdc9e776c3
 		for (String x : list) {
 			System.out.print(x + " ");
 		} System.out.println();
+		
 		Scanner in = new Scanner(System.in);
-		String user = in.nextLine();
-		if (user.equals("Ascending")) {
+		String opt = in.nextLine();
+		if (opt.equals("Ascending")) {
 			Ascending(list);
-		} else if (user.equals("Descending")) {
+		} else if (opt.equals("Descending")) {
 			Descending(list);
 		}
 	}
 	
-<<<<<<< HEAD
-	public static void Decending(ArrayList<String> list) {
-=======
 	public static void Descending(ArrayList<String> list) {
->>>>>>> f6abc7ae51ed6e2944d1b890da60f4fdc9e776c3
 		for (int i=1; i<list.size(); i++) {
 			for (int j=i; j>0 && list.get(j).compareTo(list.get(j-1)) >= 0; j--) {
 				String temp = list.get(j);
@@ -45,18 +32,12 @@ public class Ex3 {
 				list.set(j-1, temp);
 			}
 		}
-<<<<<<< HEAD
-	}
-	
-	public static void Accending(ArrayList<String> list) {
-=======
 		for (String x : list) {
 			System.out.print(x+" ");
 		} System.out.println();
 	}
 	
 	public static void Ascending(ArrayList<String> list) {
->>>>>>> f6abc7ae51ed6e2944d1b890da60f4fdc9e776c3
 		for (int i=1; i<list.size();i++) {
 			for (int j=i; j>0 && list.get(j).compareTo(list.get(j-1)) <= 0; j--) {
 				String temp = list.get(j);
@@ -64,15 +45,8 @@ public class Ex3 {
 				list.set(j-1, temp);
 			}
 		}
-<<<<<<< HEAD
-		
-		for (String x : list) {
-			System.out.print(x+" ");
-		}	
-=======
 		for (String x : list) {
 			System.out.print(x+" ");
 		} System.out.println();
->>>>>>> f6abc7ae51ed6e2944d1b890da60f4fdc9e776c3
 	}
 }
--- Unit7/src/Ex4.java
+++ Unit7/src/Ex4.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Ex4 {
 	public static void main(String[] args) throws FileNotFoundException {
-		File f = new File("C:\\Users\\yunir\\eclipse-workspace\\AP_Comp_Sci_Unit_Exercises\\Unit7\\src\\names.txt");
+		File f = new File("C:\\Users\\s-phamh\\Documents\\GitHub\\AP_Comp_Sci_Unit_Exercises\\Unit7\\src\\names.txt");
 		Scanner scan = new Scanner(f);
 		ArrayList<String> list = new ArrayList();
 		while (scan.hasNextLine()) {
@@ -22,10 +22,18 @@ public class Ex4 {
 			System.out.print(x+" ");
 		} System.out.println();
 		
-		for (int i=1; i<list.size(); i++) {
-			for (int j=i; ) {
-				
-			}
+		String r = "Ryan";
+		String n = "Sam";
+		String s = "Steve";
+		System.out.println(r.compareTo(n));
+		int index = 0;
+		for (int i=0; i<list.size() && list.get(i).compareTo(n) <= 0; i++) {
+			index = i;
 		}
+		list.add(index, n);
+		
+		for (String x : list) {
+			System.out.print(x+" ");
+		} System.out.println();
 	}
 }
