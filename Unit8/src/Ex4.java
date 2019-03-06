39d265af8682d8e2c4c454eae6bc2a831fff634c
Hannah Pham <36429830+hanpham32@users.noreply.github.com>
2019-03-06 09:29:30 -0800

    Ex4

---
+++ Unit8/src/Ex4.java
@@ -0,0 +1,31 @@
+
+public class Ex4 {
+	public static void main(String[] args){
+		System.out.println(GCD(24,18));
+	}
+	
+	public static int GCD(int a, int b) {
+		// Base case
+		if () {
+			
+		} else {
+			return a/2 * GCD(a/2, b/2); 
+		}
+	}
+}
+
+//int GCD = 0;
+//int biggest = 0;
+//if (a > b) {
+//	biggest = a;
+//} else {
+//	biggest = b;
+//}
+//for (int i=1; i<biggest; i++) {
+//	if (a%i == 0 && b%i == 0) {
+//		GCD = i;
+//	}
+//}
+//return GCD;
+
+// 
