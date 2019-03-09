6ee79d72cee82ceb0a6189d5f3c06785051e6991
Han <36429830+hanpham32@users.noreply.github.com>
2019-03-08 21:22:29 -0800

    Finished Ex4

--- Unit8/src/Ex4.java
+++ Unit8/src/Ex4.java
@@ -1,18 +1,33 @@
 
 public class Ex4 {
 	public static void main(String[] args){
-		System.out.println(GCD(24,18));
+		System.out.println(GCD(10,10));
 	}
 	
 	public static int GCD(int a, int b) {
-		// Base case
-		if () {
-			
-		} else {
-			return a/2 * GCD(a/2, b/2); 
+		for (int i = 2; i<= (a>b ? a : b); i++) {
+			if (a%i == 0 && b%i == 0) {
+				return i * GCD(a/i, b/i);
+			}
 		}
+		return 1;
 	}
 }
+
+/* FIRST ATTEMPT
+realize don't need a base case to check
+if ((a<b ? a : b) < 0) {
+	return 1;
+} else {
+	for (int i = 2; i <= (a>b ? a : b); i++) {
+		if (a%i == 0 && b%i == 0) {
+			return i * GCD(a/i, b/i);
+		}
+	}
+	return 1;
+}
+*/
+
 
 //int GCD = 0;
 //int biggest = 0;
@@ -27,5 +42,3 @@ public class Ex4 {
 //	}
 //}
 //return GCD;
-
-// 
